2524138bf488fc97a287e1a87784f16e3b491489
luyuwww@126.com <luyuwww@126.com>
2019-08-21 17:58:03 +0800

    upgrade

---
+++ docu/addField.sql
@@ -0,0 +1,8 @@
+sqlserver
+alter table s_user add ESBCODE varchar(128);
+alter table s_user add ESBID varchar(128);
+
+alter table s_group add BH varchar(128);
+alter table s_group add GFZJ varchar(128);
+alter table s_group add DEPCODE varchar(128);
+alter table s_group add DEPID varchar(128);
