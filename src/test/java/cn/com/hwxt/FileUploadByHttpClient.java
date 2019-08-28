d4e66c1d0237a6891b41634321a811ac43eaeee1
luyuwww@126.com <luyuwww@126.com>
2019-08-28 10:08:51 +0800

    一个测试用例

--- src/test/java/cn/com/hwxt/FileUploadByHttpClient.java
+++ src/test/java/cn/com/hwxt/FileUploadByHttpClient.java
@@ -1,5 +1,6 @@
 package cn.com.hwxt;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -20,43 +21,50 @@ import java.nio.charset.Charset;
  */
 public class FileUploadByHttpClient {
     public static void main(String[] args) throws Exception {
-        String fileStr = "d:/Core Java笔记MS Word版.doc";
-        File ff = new File(fileStr);
-        ContentType utf8 = ContentType.create("text/plain", Charset.forName("UTF-8"));
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        try {
-            HttpPost httppost = new HttpPost("http://localhost:8887/HamsPort/uploadEfile");
-            FileBody bin = new FileBody(ff);
+        File dir = new File("d:/data/pdf/");
+        File[] files = dir.listFiles();
+        for (File file : files) {
+            String fileStr = file.getAbsolutePath();
 
-            HttpEntity reqEntity = MultipartEntityBuilder.create()
-                    .addPart("Filedata", bin)
-                    //可能出现中文这样调用 需要 utf ,注意title不要后缀
-                    .addTextBody("title","Core Java笔记MS Word版" , utf8)
-                    //写成可以配置的正式环境和测试环境不同 测试是这个值
-                    .addTextBody("tableName","E_FILE29")
-                    //上次调用返回的did
-                    .addTextBody("fileDID","11")
-                    //写成可以配置的正式环境和测试环境不同
-                    .addTextBody("pzm","ddata")
-                    //按照业务确定
-                    .addTextBody("upUserCode","ROOT" , utf8)
-                    .build();
-            httppost.setEntity(reqEntity);
-            System.out.println("executing request " + httppost.getRequestLine());
-            CloseableHttpResponse response = httpclient.execute(httppost);
+            System.out.println(fileStr);
+            File ff = new File(fileStr);
+            ContentType utf8 = ContentType.create("text/plain", Charset.forName("UTF-8"));
+            CloseableHttpClient httpclient = HttpClients.createDefault();
             try {
-                System.out.println(response.getStatusLine());
-                HttpEntity resEntity = response.getEntity();
-                if (resEntity != null) {
-                    System.out.println("Response content length: " + resEntity.getContentLength());
+                HttpPost httppost = new HttpPost("http://10.185.101.4/HamsPort/uploadEfile");
+                FileBody bin = new FileBody(ff);
+
+                HttpEntity reqEntity = MultipartEntityBuilder.create()
+                        .addPart("Filedata", bin)
+                        //可能出现中文这样调用 需要 utf ,注意title不要后缀
+                        .addTextBody("title", FilenameUtils.getBaseName(file.getName()), utf8)
+                        //写成可以配置的正式环境和测试环境不同 测试是这个值
+                        .addTextBody("tableName","E_FILE29")
+                        //上次调用返回的did
+                        .addTextBody("fileDID","4")
+                        //写成可以配置的正式环境和测试环境不同
+                        .addTextBody("pzm","ddata")
+                        //按照业务确定
+                        .addTextBody("upUserCode","ROOT" , utf8)
+                        .build();
+                httppost.setEntity(reqEntity);
+                System.out.println("executing request " + httppost.getRequestLine());
+                CloseableHttpResponse response = httpclient.execute(httppost);
+                try {
+                    System.out.println(response.getStatusLine());
+                    HttpEntity resEntity = response.getEntity();
+                    if (resEntity != null) {
+                        System.out.println("Response content length: " + resEntity.getContentLength());
+                    }
+                    EntityUtils.consume(resEntity);
+                } finally {
+                    response.close();
                 }
-                EntityUtils.consume(resEntity);
             } finally {
-                response.close();
+                httpclient.close();
             }
-        } finally {
-            httpclient.close();
         }
+
     }
 }
 
