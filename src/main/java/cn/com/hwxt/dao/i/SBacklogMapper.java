27d2e39f6707ee3f9ce7fadd4a0305676b950ed0
luyuwww@126.com <luyuwww@126.com>
2019-12-08 23:38:46 +0800

    upgrade hasDone

---
+++ src/main/java/cn/com/hwxt/dao/i/SBacklogMapper.java
@@ -0,0 +1,38 @@
+package cn.com.hwxt.dao.i;
+
+import cn.com.hwxt.dao.BaseDao;
+import cn.com.hwxt.pojo.SBacklog;
+import cn.com.hwxt.pojo.SBacklogExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+public interface SBacklogMapper  extends BaseDao {
+    int countByExample(SBacklogExample example);
+
+    int deleteByExample(SBacklogExample example);
+
+    int deleteByPrimaryKey(Integer did);
+
+    int insert(SBacklog record);
+
+    int insertSelective(SBacklog record);
+
+    List<SBacklog> selectByExample(SBacklogExample example);
+
+    SBacklog selectByPrimaryKey(Integer did);
+
+    int updateByExampleSelective(@Param("record") SBacklog record, @Param("example") SBacklogExample example);
+
+    int updateByExample(@Param("record") SBacklog record, @Param("example") SBacklogExample example);
+
+    int updateByPrimaryKeySelective(SBacklog record);
+
+    int updateByPrimaryKey(SBacklog record);
+
+    @Select("SELECT COUNT(*) FROM S_BACKLOG WHERE ${whereSql}")
+    Integer getBackLogNum(@Param("whereSql") String whereSql);
+
+    @Select("SELECT * FROM S_BACKLOG WHERE BZ IN (SELECT BZ FROM S_BACKLOG WHERE ACTTASKID='${taskId}' GROUP BY BZ)")
+    List<SBacklog> getAllBacklogOnProcess(@Param("taskId") String taskId);
+}
